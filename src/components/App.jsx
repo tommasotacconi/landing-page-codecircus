30c79547026973ef9bc2cb22c11d4553da1f501a
GiorgioDordo <djole6789@gmail.com>
2025-03-04 12:58:42 +0100

    refactor: restructure project by removing old files and adding new components

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Hello from './Greatings.jsx';
+import '../App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+      <Hello />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Greatings.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../Greatings.css';
+
+function Greatings() {
+  return (
+    <div className="greatings" >Greatings</div>
+  );
+}
+
+export default Greatings;
